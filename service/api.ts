2f4a3519470e1e25a35dc6e8192d1425a127702c
karlacabanas01 <karla@kranio.io>
2025-02-10 20:41:29 -0300

    update route

--- service/api.ts
+++ service/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://3.142.76.216:3000/api", // Asegúrate de que el backend esté corriendo aquí
+  baseURL: "http://3.23.94.254:3000/api", // Asegúrate de que el backend esté corriendo aquí
 });
 
 export default api;
