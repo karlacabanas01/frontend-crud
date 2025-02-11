485341ecdb0c0c2543d36fc4d5446279670f7967
karlacabanas01 <karla@kranio.io>
2025-02-10 21:15:38 -0300

    update api

--- service/api.ts
+++ service/api.ts
@@ -1,7 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://3.23.94.254:3000/api", // Asegúrate de que el backend esté corriendo aquí
+  baseURL: "http://3.23.94.254:3000/api",
 });
+
+export const login = async (email: any, password: any) => {
+  try {
+    const response = await api.post("/auth/login", { email, password });
+    console.log("Login exitoso:", response.data);
+    return response.data;
+  } catch (error: any) {
+    console.error("Error en el login:", error.response?.data || error.message);
+    throw error;
+  }
+};
 
 export default api;
